27a82c5ff20e35c0d751f9c6faeabfbca44edc32
young1-developer <usamasaniabdullahi814@gmail.com>
2023-08-09 11:04:19 +0100

    adding the updated game with UI

--- main.js
+++ main.js
@@ -1,48 +1,95 @@
+
+let i = 0;
+let txt = 'Rock, Paper, Scissors?';
+let speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("gameh1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+        
+    }
+}
+
+
+
+var playerScore = 0;
+var computerScore = 0;
+
 function computerMove() {
-    const moves = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random() * moves.length);
-    return moves[randomIndex];
+  const moves = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * moves.length);
+  return moves[randomIndex];
+}
+
+function determineWinner(userMove, computerMove) {
+  if (userMove === computerMove) {
+    return 'tie';
+  } else if (
+    (userMove === 'rock' && computerMove === 'scissors') ||
+    (userMove === 'paper' && computerMove === 'rock') ||
+    (userMove === 'scissors' && computerMove === 'paper')
+  ) {
+    playerScore++;
+    document.getElementById('playerScore').textContent = playerScore;
+    return 'user';
+  } else {
+    computerScore++;
+    document.getElementById('computerScore').textContent = computerScore;
+    return 'computer';
   }
-  
-  function getUserMove() {
-    let userMove = prompt("Enter your move (rock, paper, or scissors):").toLowerCase();
-    const validMoves = ['rock', 'paper', 'scissors'];
-  
-    while (!validMoves.includes(userMove)) {
-      userMove = prompt("Invalid move. Please enter rock, paper, or scissors:").toLowerCase();
+}
+
+const choiceButtons = document.querySelectorAll(".choice");
+const resultDiv = document.getElementById("output");
+
+choiceButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (!GameOver()) {
+      const userMoveInput = button.id;
+      const computerMoveResult = computerMove();
+      const winner = determineWinner(userMoveInput, computerMoveResult);
+
+      resultDiv.textContent = `You chose: ${userMoveInput}\n\n;  Computer chose: ${computerMoveResult}\n; `;
+
+      if (winner === 'tie') {
+        resultDiv.textContent += "It's a tie!";
+      } else {
+        resultDiv.textContent += `${winner === 'user' ? 'You win' : 'Computer wins!'}`;
+      }
+
+      if (GameOver()) {
+        resultDiv.textContent += `\n\n${playerScore === 5 ? 'You have won!' : 'Computer has won!'}`;
+      }
     }
+  });
+});
+
+// function to check if playerScore is greater than or equal to 5
+function GameOver(){
+  return  playerScore >= 5  || computerScore >= 5;
   
-    return userMove;
-  }
+}
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', resetFunc);
+// Reset the game
+function resetFunc() {
+  playerScore = 0;
+  computerScore = 0;
+  document.getElementById('playerScore').textContent = playerScore;
+  document.getElementById('computerScore').textContent = computerScore;
+  resultDiv.textContent = "who score 5 point first win the game"; // Clear the result display
+  userMoveInput = false;
+  computerMoveResult = false; // Fix: Set computerMoveResult to false
   
-  function determineWinner(userMove, computerMove) {
-    if (userMove === computerMove) {
-      return 'tie';
-    } else if (
-      (userMove === 'rock' && computerMove === 'scissors') ||
-      (userMove === 'paper' && computerMove === 'rock') ||
-      (userMove === 'scissors' && computerMove === 'paper')
-    ) {
-      return 'user';
-    } else {
-      return 'computer';
-    }
-  }
   
-  function playGame() {
-    let userMoveInput = getUserMove();
-    let computerMoveResult = computerMove();
-    let winner = determineWinner(userMoveInput, computerMoveResult);
-  
-    console.log(`You chose: ${userMoveInput}`);
-    console.log(`Computer chose: ${computerMoveResult}`);
-  
-    if (winner === 'tie') {
-      console.log("It's a tie!");
-    } else {
-      console.log(`${winner === 'user' ? 'You win!' : 'Computer wins!'}`);
-    }
-  }
-  
-  playGame();
-  
+}
+
+
+typeWriter();
+
+
+
+
+
